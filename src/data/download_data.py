d95ce2e1d515258c92320ebfa8006a924e56d853
hamidmajidibalanji <majidi.balanji.hamid@gmail.com>
2026-05-06 21:33:24 +0300

    update the download_data.py to download the main data which is humaid with whole size which is above 70000 tweets and test the download_data.py

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,27 +1,35 @@
 # Data Download Module  
 # First: test with a small dataset, then scale up to the full dataset
+# Second: Replace the sample dataset URL with the actual dataset URL containing earthquake tweets.
 
 import os  
 import pandas as pd
 
-DATA_URL_ADDRESS = "https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json"   
+# Sample dataset to check whether dataset donwloading works.   
+# DATA_URL_ADDRESS = "https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json"   
 
-SAVE_DATA_PATH = "data/raw/data.csv" 
+SAVE_DATA_PATH = "data/raw/humaid.csv" 
 
 
 # Definition of the function to download the data   
 def download_data():
     os.makedirs("data/raw", exist_ok=True)     
     
-    print("Downloading dataset...")
+    print("Downloading HumAID dataset...")
     
     # Sample dataset: It would be replaced with the actual dataset with earthquake tweets.    
-    df = pd.read_json(DATA_URL_ADDRESS)
+    dataset = load_dataset("Firoj/HumAID", split="train")
+
+    df = pd.DataFrame(dataset)  
+    
+    #Keep only the relevant columns
+    df = df[["tweet_text", "class_label"]]
     
     # converting json format into csv.   
     df.to_csv(SAVE_DATA_PATH, index=False)
     
-    print(f"Dataset downloaded and saved to {SAVE_DATA_PATH}")   
+    print(f"Dataset downloaded and saved to {SAVE_DATA_PATH}")  
+    print(df.head())  # Print the first few rows of the dataset to verify   
     
     
     
