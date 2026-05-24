235ff8ce4d803e560d781cd2dd8ba134dba07caa
hamidmajidibalanji <majidi.balanji.hamid@gmail.com>
2026-05-24 21:14:26 +0300

    introduced second method for the classification called SVM

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,17 +1,76 @@
+# Train Pipeline for Disaster Tweets Classification
+
 import pandas as pd  
-import joblib  
+import joblib
+import os  
 
 from sklearn.model_selection import train_test_split
+
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, accuracy_score   
+
+from sklearn.svm import LinearSVC   
+
+from sklearn.metrics import ( 
+    classification_report, 
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,   
+    classification_report
+)
+   
 from sklearn.pipeline import Pipeline
+
+
 
 
 INPUT_PATH = "data/processed/processed_disaster_tweets.csv"
 
+MODELS_DIR = "models"
+
 MODEL_PATH = "models/logistic_regression.pkl"
 VECTORIZER_PATH = "models/tfidf_vectorizer.pkl"
+
+
+os.makedirs(MODELS_DIR, exist_ok=True)   
+
+
+def evaluate_model(model, X_test, y_test, model_name):
+    
+    predictions = model.predict(X_test)  
+    
+    accuracy = accuracy_score(y_test, predictions)
+    precision = precision_score(y_test, predictions)
+    recall = recall_score(y_test, predictions)
+    f1 = f1_score(y_test, predictions) 
+    
+    
+    
+    
+    print("\n")
+    print("=" * 50)  
+    print(f"{model_name} RESULTS")  
+    print("=" * 50)  
+    
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"F1-Score: {f1:.4f}")
+    
+    print("\nClassification Report:")
+    print(classification_report(y_test, predictions))
+    
+    
+    return {
+        "Model": model_name,
+        "Accuracy": accuracy,
+        "Precision": precision,
+        "Recall": recall,
+        "F1-Score": f1
+    }
+
 
 
 
@@ -32,31 +91,85 @@ def train_model():
     vectorizer = TfidfVectorizer(stop_words='english', max_features=5000) 
     
     X_train_tfidf = vectorizer.fit_transform(X_train)
-    X_test_tfidf = vectorizer.transform(X_test)
+    X_test_tfidf = vectorizer.transform(X_test)    
+    
+    joblib.dump(
+        vectorizer,
+        os.path.join(MODELS_DIR, "tfidf_vectorizer.pkl")
+    )
+    
+    results = []   
+    
+    # --------------------------------------------------
+    #  Logistic Regression
+    # --------------------------------------------------    
+    
 
     print("Training Logistic Regression model...")
 
-    model = LogisticRegression(max_iter=1000)
+    lr_model = LogisticRegression(max_iter=1000)
 
-    model.fit(X_train_tfidf, y_train)
+    lr_model.fit(X_train_tfidf, y_train)
+    
+    joblib.dump(
+        lr_model,
+        os.path.join(MODELS_DIR, "logistic_regression.pkl")
+    )
+    
+    lr_results = evaluate_model(
+        lr_model, X_test_tfidf,
+        y_test, 
+        "Logistic Regression")
+    
+    
+    results.append(lr_results)  
+    
+    # --------------------------------------------------
+    #  Linear SVM
+    # --------------------------------------------------  
+    print("\nTraining Linear SVM model...")
+    
+    svm_model = LinearSVC()  
+    
+    svm_model.fit(X_train_tfidf, y_train)   
+    
+    joblib.dump(
+        svm_model,
+        os.path.join(MODELS_DIR, "svm_model.pkl")
+    )
+    
+    
+    svm_results = evaluate_model(
+        svm_model, 
+        X_test_tfidf,
+        y_test, 
+        "SVM"
+    )
+    
+    results.append(svm_results)
+    
+    # --------------------------------------------------
+    #  Save Comparison REsults   
+    # --------------------------------------------------
+    
+    results_df = pd.DataFrame(results)  
+    
+    os.makedirs("outputs", exist_ok=True)   
+    
+    results_df.to_csv(
+        "outputs/model_comparison.csv", 
+        index=False
+    )
+    
+    
+    print("\nSaved model comparison results.")
+    
+    print("\nTraining complete. Models and vectorizer saved to disk.")  
+    
+    
+    
+    
 
-    print("Evaluating model...")
-
-    predictions = model.predict(X_test_tfidf)
-
-    report = classification_report(y_test, predictions)
-
-    print(report)
-
-    print("Saving model...")
-
-    joblib.dump(model, MODEL_PATH)
-    joblib.dump(vectorizer, VECTORIZER_PATH)
-
-    print("Training complete.")
-    print(f"Model saved to: {MODEL_PATH}")
-    print(f"Vectorizer saved to: {VECTORIZER_PATH}")
-
-
+    
 if __name__ == "__main__":
     train_model()
